814de290259188af534f5e89208ff2752af87289
Ben Kristinsson <ben@sudo.is>
2023-03-17 00:20:23 +0100

    fixes

--- owntone/.pipeline/build.sh
+++ owntone/.pipeline/build.sh
@@ -11,7 +11,7 @@ rm -rv ./dist/target || true
 rm -v ./dist/*.tar.gz || true
 rm -v ./dist/*.deb || true
 
-docker build --target builder -t owntone:latest-builder .
+docker build --pull --target builder -t owntone:latest-builder .
 docker run -u $(id -u) --name owntone-build --rm -it -v $(pwd)/dist/:/mnt/dist/ owntone:latest-builder cp -r /usr/local/src/dist/. /mnt/dist/
 
-docker build -t ${DOCKER_REPO}/owntone:latest .
+docker build --pull -t ${DOCKER_REPO}/owntone:latest .
