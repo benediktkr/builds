05fe010f915395302860c0b455317509e34e49fe
Ben Kristinsson <ben@sudo.is>
2023-03-17 01:14:38 +0100

    more fixes

--- owntone/.pipeline/docker-run.sh
+++ owntone/.pipeline/docker-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker run -v /run/dbus:/run/dbus --net=host --name owntone --rm -it git.sudo.is/ben/owntone:latest
+#docker run --net=host --name owntone --rm -it git.sudo.is/ben/owntone:latest $*
+mkdir -p /tmp/owntone-test
+docker run -v /tmp/owntone-test:/srv/music -v $(pwd)/docker/owntone.conf:/etc/owntone.conf -p 3689:3689 --name owntone --rm -it git.sudo.is/ben/owntone:latest $*
 
 
--- owntone/entrypoint.sh
+++ owntone/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-mkdir /var/cache/owntone
-chown owntone:owntone /var/cache/owntone
 
 /etc/init.d/dbus start
 /etc/init.d/avahi-daemon start
-/usr/sbin/owntone -f
+
+/usr/sbin/owntone $*
