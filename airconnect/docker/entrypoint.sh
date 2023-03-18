4314979f5ec26fa72458754e05d889edf3adcf9b
Ben Kristinsson <ben@sudo.is>
2023-03-18 21:54:53 +0100

    fix typo in config file path

--- airconnect/docker/entrypoint.sh
+++ airconnect/docker/entrypoint.sh
@@ -10,6 +10,11 @@ usage() {
     exit 1
 }
 
+if [ "$AIRCONNECT_AIRUPNP_CONFIG" = "" ]; then
+    # set in Dockerfile, so can be changed on runtime
+    AIRCONNECT_AIRUPNP_CONFIG="/etc/airupnp.xml"
+fi
+
 # set PROG and ARGS vars
 if [ -n "$1" ]; then
     # set PROG and ARGS from commandline if given
@@ -33,7 +38,7 @@ fi
 if [ "$ARGS" = "" ]; then
     if [ "$PROG" = "airupnp" ]; then
         # set default args for airupnp if not set
-        ARGS="-x /etx/airupnp.xml"
+        ARGS="-x $AIRCONNECT_AIRUPNP_CONFIG"
 
     elif [ "$PROG" = "aircast" ]; then
         # no default args for aircast
@@ -47,12 +52,20 @@ env | grep "AIRCONNECT_"
 echo
 echo "[ ] entrypoint"
 echo "    PROG=${PROG}"
-echo "    ARGS=${ARGS}"
+echo "    ARGS=\"${ARGS}\""
 echo
 echo "[ ] running as:"
 echo "    whoami: $(whoami)"
 echo "    id: $(id)"
 echo
+
+if [ "$PROG" = "airupnp" ]; then
+    echo "[ ] config file:"
+    echo "    AIRCONNECT_AIRUPNP_CONFIG=$AIRCONNECT_AIRUPNP_CONFIG"
+    echo "    contents:"
+    cat $AIRCONNECT_AIRUPNP_CONFIG
+    echo
+fi
 
 set -x
 /usr/local/bin/${PROG} $ARGS
