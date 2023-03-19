933db0a24e317e7d7ff8700e56e866cb03622578
Ben Kristinsson <ben@sudo.is>
2023-03-19 19:04:45 +0100

    actually source the var

--- owntone/.pipeline/build.sh
+++ owntone/.pipeline/build.sh
@@ -3,6 +3,12 @@
 set -e
 
 source ./.pipeline/owntone.env
+source ./.pipeline/owntone-url.env
+
+if [ "$VITE_OWNTONE_URL" = "" ]; then
+    echo "VITE_OWNTONE_URL is not set, default will be used!"
+    sleep 10
+fi
 
 . ./.pipeline/init-git.sh
 
